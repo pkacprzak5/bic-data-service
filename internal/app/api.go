1e6b3cc072bf59c9a602a38a1a09da078683b143
Piotr Kacprzak <piotrkac4@gmail.com>
2025-01-21 20:56:53 +0100

    Routing added

--- internal/app/api.go
+++ internal/app/api.go
@@ -1,1 +1,55 @@
 package app
+
+import (
+	"context"
+	"github.com/pkacprzak5/bic-data-service/internal/storage"
+	"log"
+	"net/http"
+	"time"
+)
+
+type APIServer struct {
+	address string
+	storage storage.Storage
+}
+
+func NewAPIServer(address string, storage storage.Storage) *APIServer {
+	return &APIServer{address: address, storage: storage}
+}
+
+func (s *APIServer) Start(ctx context.Context) error {
+	router := http.NewServeMux()
+	subrouter := http.NewServeMux()
+
+	router.Handle("/v1/", http.StripPrefix("/v1/", subrouter))
+
+	bankService := NewBankService(s.storage)
+	bankService.RegisterRoutes(subrouter)
+
+	server := &http.Server{
+		Addr:    s.address,
+		Handler: subrouter,
+	}
+
+	ch := make(chan error, 1)
+	go func() {
+		log.Println("Starting API server at", s.address)
+
+		err := server.ListenAndServe()
+		if err != nil {
+			ch <- err
+		}
+		close(ch)
+	}()
+
+	select {
+	case err := <-ch:
+		return err
+	case <-ctx.Done():
+		log.Println("Received shutdown signal, shutting down the server...")
+		timeout, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		return server.Shutdown(timeout)
+	}
+}
