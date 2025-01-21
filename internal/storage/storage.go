fc13298f9505cc7ac35c2206015496039da61ab6
Piotr Kacprzak <piotrkac4@gmail.com>
2025-01-21 20:16:40 +0100

    Types created

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,1 +1,11 @@
 package storage
+
+type Storage interface {
+	GetSwiftCodeDetails(swiftCode string) (Bank, error)
+
+	GetSwiftCodesForCountry(iso2Code string) (CountryBanks, error)
+
+	AddSwiftCodeEntry(b Bank) error
+
+	DeleteSwiftCodeEntry(swiftCode string) error
+}
---
+++ internal/storage/types.go
@@ -0,0 +1,25 @@
+package storage
+
+type Bank struct {
+	Address       string       `json:"address"`
+	BankName      string       `json:"bankName"`
+	CountryISO2   string       `json:"countryISO2"`
+	CountryName   string       `json:"countryName"`
+	IsHeadquarter bool         `json:"isHeadquarter"`
+	SwiftCode     string       `json:"swiftCode"`
+	Branches      []BankBranch `json:"branches"`
+}
+
+type BankBranch struct {
+	Address       string `json:"address"`
+	BankName      string `json:"bankName"`
+	CountryISO2   string `json:"countryISO2"`
+	IsHeadquarter bool   `json:"isHeadquarter"`
+	SwiftCode     string `json:"swiftCode"`
+}
+
+type CountryBanks struct {
+	CountryISO2 string       `json:"countryISO2"`
+	CountryName string       `json:"countryName"`
+	SwiftCodes  []BankBranch `json:"swiftCode"`
+}
